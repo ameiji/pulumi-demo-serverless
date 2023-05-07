a9aee4fe93db78c8445e3e6aa3e03d1ac0ac4c56
dsarkisov <ait.meijin@gmail.com>
2023-05-07 13:55:22 +0200

    Add Resource functions

--- __main__.py
+++ __main__.py
@@ -3,19 +3,8 @@
 import pulumi
 
 from api_gateway import create_api_gateway
-from lambda_functions import create_lambda_function
-from iam import create_lambda_exec_role
 
-# api_id = create_api_gateway()
+api_id, invoke_url = create_api_gateway()
 
-role_arn = create_lambda_exec_role("lambda-exec-role", assume_policy_filename="lambdas/execution_role.json")
-
-func = create_lambda_function(name="test_lambda",
-                              filename="./lambdas/test_lambda.py",
-                              runtime="python3.9",
-                              role_arn=role_arn,
-                              description="Test python lambda function"
-                              )
-
-
-pulumi.export('rest_api id', func.arn)
+pulumi.export("REST API id", api_id)
+pulumi.export("Invoke URL", invoke_url)
--- lambda_functions.py
+++ lambda_functions.py
@@ -1,42 +1,31 @@
-import os
-
+from typing import Optional, Mapping
 import pulumi
 import pulumi_aws as aws
-from typing import Optional
-from zipfile import ZipFile
-
-zip_files_list = []
-
-def get_zip_filename(filename: str) -> str:
-    zip_files_list.append(f"{filename}.zip")
-    return f"{filename}.zip"
+from iam import create_lambda_exec_role
 
 
-def create_zip_archive(filename: str) -> str:
-    with ZipFile(get_zip_filename(filename), 'w') as zip_object:
-        zip_object.write(filename)
-    return get_zip_filename(filename)
+DEFAULT_LAMBDA_ROLE_ARN = create_lambda_exec_role(
+    "lambda-exec-role", assume_policy_filename="lambdas/execution_role.json"
+)
 
 
-def create_file_archive(filename: str) -> pulumi.Archive:
-    zip_filename = create_zip_archive(filename)
-    return pulumi.FileArchive(zip_filename)
-
-
-# this is the core function
-def create_lambda_function(name: str,
-                           filename: str,
-                           runtime: str,
-                           role_arn: str,
-                           description: Optional[str] = None
-                           ) -> aws.lambda_.Function:
-    func = aws.lambda_.Function(name,
-                                name=name,
-                                runtime=runtime,
-                                role=role_arn,
-                                handler="lambda_handler",
-                                code=create_file_archive(filename),
-                                description=description
-                                )
+def create_lambda_function(
+    name: str,
+    filename: str,
+    runtime: str,
+    handler: str,
+    role_arn: str = DEFAULT_LAMBDA_ROLE_ARN,
+    environment: Mapping[str, str] = None,
+    description: Optional[str] = None,
+) -> aws.lambda_.Function:
+    func = aws.lambda_.Function(
+        name,
+        name=name,
+        runtime=runtime,
+        role=role_arn,
+        handler=handler,
+        code=pulumi.asset.AssetArchive({"folder": pulumi.FileArchive(filename)}),
+        description=description,
+        environment=environment,
+    )
     return func
-
