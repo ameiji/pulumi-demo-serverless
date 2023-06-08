d6b65c1b6e58a6144b82130479cf050bdbe0a1b1
Pavel Salnikov <psalnikov@apple.com>
2023-06-08 14:51:30 +0200

    Part 3. Ready app without auth

---
+++ 03-deploy-cf-s3/build-frontend.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -eux -o pipefail
+
+gpulumi="$HOME/Downloads/pulumi/pulumi"
+gpulumi="$HOME/apps/workshop/bin/pulumi/pulumi"
+STACK_NAME="pulumi-demo-serverless"
+
+# Create build dir
+BUILD_DIR=/tmp/build
+
+cd ..
+mkdir -p "$BUILD_DIR"
+rm -rf frontend-src/build
+cp -rv frontend-src "$BUILD_DIR"
+
+# Get stack outputs
+$gpulumi stack select "$STACK_NAME"
+$gpulumi stack output -j > pulumi_stack_output.json
+
+AWS_USER_POOLS_WEB_CLIENT_ID=$( cat pulumi_stack_output.json | jq '.aws_user_pools_web_client_id')
+API_BASE_URL=$( cat pulumi_stack_output.json | jq '.backend_invoke_url')
+STAGE_NAME_PARAM=$( cat pulumi_stack_output.json | jq '.stage_name')
+COGNITO_HOSTED_DOMAIN=$( cat pulumi_stack_output.json | jq '.cognito_custom_domain')
+REDIRECT_URL=$( cat pulumi_stack_output.json | jq '.website_url')
+
+rm pulumi_stack_output.json
+
+# Copy config
+pushd "$BUILD_DIR/frontend-src"
+cp -fv src/config.default.js src/config.js
+
+# Build React app
+npm install
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -e 's/AWS_USER_POOLS_WEB_CLIENT_ID/'"$AWS_USER_POOLS_WEB_CLIENT_ID"'/g' src/config.js
+  sed -i '' -e 's/API_BASE_URL/'"${API_BASE_URL//\//\\/}"'/g' src/config.js
+  sed -i '' -e 's/{StageNameParam}/'"$STAGE_NAME_PARAM"'/g' src/config.js
+  sed -i '' -e 's/COGNITO_HOSTED_DOMAIN/'"$COGNITO_HOSTED_DOMAIN"'/g' src/config.js
+  sed -i '' -e 's/REDIRECT_URL/'"${REDIRECT_URL//\//\\/}"'/g' src/config.js
+else
+  sed -i -e 's/AWS_USER_POOLS_WEB_CLIENT_ID/'"$AWS_USER_POOLS_WEB_CLIENT_ID"'/g' src/config.js
+  sed -i -e 's/API_BASE_URL/'"${API_BASE_URL//\//\\/}"'/g' src/config.js
+  sed -i -e 's/{StageNameParam}/'"$STAGE_NAME_PARAM"'/g' src/config.js
+  sed -i -e 's/COGNITO_HOSTED_DOMAIN/'"$COGNITO_HOSTED_DOMAIN"'/g' src/config.js
+  sed -i -e 's/REDIRECT_URL/'"${REDIRECT_URL//\//\\/}"'/g' src/config.js
+fi
+
+npm run build
+popd
+
+# Copy sources
+cp -rv "$BUILD_DIR/frontend-src/build" frontend-src/
+
+echo "=> Build complete"
+echo "=> Now you can run 'pulumi up' to upload frontend app sources to S3."
+
